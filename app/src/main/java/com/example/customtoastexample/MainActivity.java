cd96892d644751f7042a2d1938744ed2eececc72
sunil <sunilkhedkar22215@gmail.com>
2020-11-12 10:51:26 +0530

    mainactivity java

--- app/src/main/java/com/example/customtoastexample/MainActivity.java
+++ app/src/main/java/com/example/customtoastexample/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.customtoastexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LayoutInflater li=getLayoutInflater();
+        View layout=li.inflate(R.layout.customtoast,(ViewGroup)findViewById(R.id.custom_toast_layout));
+
+        Toast toast=new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+        toast.setView(layout);
+        toast.show();
     }
 }
